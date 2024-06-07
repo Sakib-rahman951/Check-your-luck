cb13dce8e19158a908afaaef2c400cbde776b1d3
Sakib Rahman <89739577+Sakib-rahman951@users.noreply.github.com>
2024-06-07 23:28:13 +0300

    Add files via upload

---
+++ check your luck.py
@@ -0,0 +1,21 @@
+print ("ᗯEᒪᒪᑕOᗰE TO Oᑌᖇ ᑎEᗯ ᑭᖇOᒍEᑕT ")
+print (       "check your luck numb  ")
+print ("create by")
+print ("MAN1")
+
+veriable1 = input("Type your 1st luck Number")
+veriable2 = input("Now choose your 2nd NUmber")
+veriable3 = input("Final luck number")
+sum = int(veriable1)+int(veriable2)+int(veriable3)
+print (sum)
+
+veriable4 = sum
+
+if veriable4 <= 1001:
+    print ("TOᗪᗩY YOᑌ ᗩᖇE ᒪᑌᑕKY")
+
+elif veriable4 > 100000:
+    print ("𝐘𝐎𝐔 𝐀𝐑𝐄 𝐒𝐎 𝐔𝐍𝐋𝐔𝐂𝐊𝐘 𝐭𝐨𝐝𝐚𝐲")
+
+else:
+    print ("Oops, 𝐼 𝒽𝒶𝓋𝑒 𝓃☯ 𝒾𝒹𝑒𝒶")
